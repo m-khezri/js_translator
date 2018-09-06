4a2b5a8afb21cf2fcce00e37e680321d63f9f21f
mohammad <khezri@outlook.com>
2018-09-06 12:39:15 -0500

    minor improvements

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
 let spain {
-    "merry":"alegre",
-    "christmas":"Navidad",
-    "and":"y",
-    "happy":"contento",
-    "new":"nuevo",
-    "year":"año"
+    merry:"alegre",
+    christmas:"Navidad",
+    and:"y",
+    happy:"contento",
+    new:"nuevo",
+    year:"año"
 };
 
 let german {
-    "merry":"fröhlich",
-    "christmas":"Weihnachten",
-    "and":"und",
-    "happy":"glücklich",
-    "new":"Neu",
-    "year":"Jahr"
+    merry:"fröhlich",
+    christmas:"Weihnachten",
+    and:"und",
+    happy:"glücklich",
+    new:"Neu",
+    year:"Jahr"
 };
 
 let french {
-    "merry":"joyeux",
-    "christmas":"Noël",
-    "and":"et",
-    "happy":"content",
-    "new":"Nouveau",
-    "year":"an"
+    merry:"joyeux",
+    christmas:"Noël",
+    and:"et",
+    happy:"content",
+    new:"Nouveau",
+    year:"an"
 };
 
