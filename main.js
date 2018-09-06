99df3a76c50d06c5110641882c5395c866b1e489
mohammad <khezri@outlook.com>
2018-09-05 19:56:58 -0500

    creating the objects

---
+++ main.js
@@ -0,0 +1,27 @@
+let spain {
+    "merry":"alegre",
+    "christmas":"Navidad",
+    "and":"y",
+    "happy":"contento",
+    "new":"nuevo",
+    "year":"año"
+};
+
+let german {
+    "merry":"fröhlich",
+    "christmas":"Weihnachten",
+    "and":"und",
+    "happy":"glücklich",
+    "new":"Neu",
+    "year":"Jahr"
+};
+
+let french {
+    "merry":"joyeux",
+    "christmas":"Noël",
+    "and":"et",
+    "happy":"content",
+    "new":"Nouveau",
+    "year":"an"
+};
+
