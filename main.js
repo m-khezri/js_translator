644abf5c109f630df91c58f1d453c2a5bc3d6de5
mohammad <khezri@outlook.com>
2018-09-10 00:26:12 -0500

    funished project

--- main.js
+++ main.js
@@ -1,42 +1,90 @@
-let spainish = {
-    merry:"alegre",
-    christmas:"Navidad",
-    and:"y",
-    happy:"contento",
-    new:"nuevo",
-    year:"año"
+const spanish = {
+    "merry": "Feliz",
+    "christmas": "Navidad",
+    "and": "y",
+    "happy": "prospero",
+    "new": "nuevo",
+    "year": "ano"
 };
 
-let german = {
-    merry:"fröhlich",
-    christmas:"Weihnachten",
-    and:"und",
-    happy:"glücklich",
-    new:"Neu",
-    year:"Jahr"
+const german = {
+    "merry": "Frohe",
+    "christmas": "Weihnachten",
+    "and": "und ein",
+    "happy": "gluckliches",
+    "new": "Neues",
+    "year": "Jahr"
 };
 
-let french = {
-    merry:"joyeux",
-    christmas:"Noël",
-    and:"et",
-    happy:"content",
-    new:"Nouveau",
-    year:"an"
+const french = {
+    "merry": "joyeux",
+    "christmas": "Noel",
+    "and": "et",
+    "happy": "bonne",
+    "new": "nouvel",
+    "year": "An"
 };
 
-const x =1;
+const printToDom = (stringToPrint, divId) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += stringToPrint;
+};
+      
+const spanishTranslate = () => {
 
+    const inputValue = document.getElementById('inputBox').value;
+    const inputWords = inputValue.split(" ");
 
-const spanish_tr =() => {
-    var x = document.getElementById("text_area").value;
-    
-    for (let i=0; i <=spanish.length; i++) {
-        document.getElementById("output_box").innerHTML = spanish[i];
-        i+=1;
-
-    }
-    
-
+    const spanishTranslatedString = spanishStringBuilder (inputWords);
+    printToDom(spanishTranslatedString, 'returnText');
 }
 
+const spanishStringBuilder  = (inputWords) => {
+    let newString = '';
+    for(let i = 0; i < inputWords.length; i++) {
+        let spanishWord = spanish[inputWords[i]];
+        newString += `<p>${spanishWord}</p>`
+
+    };
+
+    return newString;
+
+}
+const spanishButton = document.getElementById('spanishButton');
+    spanishButton.addEventListener("click", spanishTranslate);
+
+    const germanTranslate = () => {
+        const inputValue = document.getElementById('inputBox').value;
+        const inputWords = inputValue.split(" ");
+        const germanTranslatedString = germanStringBuilder (inputWords);
+        printToDom(germanTranslatedString, 'returnText');
+    }
+    
+    const germanStringBuilder  = (inputWords) => {
+        let newString = '';
+        for(let i = 0; i < inputWords.length; i++) {
+            let germanWord = german[inputWords[i]];
+            newString += `<p>${germanWord}</p>`
+        };
+        return newString;
+    }
+    const germanButton = document.getElementById('germanButton');
+        germanButton.addEventListener("click", germanTranslate);
+
+        const frenchTranslate = () => {
+            const inputValue = document.getElementById('inputBox').value;
+            const inputWords = inputValue.split(" ");
+            const frenchTranslatedString = frenchStringBuilder (inputWords);
+            printToDom(frenchTranslatedString, 'returnText');
+        }
+        
+        const frenchStringBuilder  = (inputWords) => {
+            let newString = '';
+            for(let i = 0; i < inputWords.length; i++) {
+                let frenchWord = french[inputWords[i]];
+                newString += `<p>${frenchWord}</p>`
+            };
+            return newString;
+        }
+        const frenchButton = document.getElementById('frenchButton');
+            frenchButton.addEventListener("click", frenchTranslate);
